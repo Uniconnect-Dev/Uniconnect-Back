111e2d51dddca57630d350f5602a66950d42832f
eastlight82 <bigstake82@gmail.com>
2026-01-08 14:18:31 +0900

    feat:/payment 쇼핑몰, /signup 비이메일 구현

--- src/main/java/com/uniConnect/member/security/local/dto/AuthDto.java
+++ src/main/java/com/uniConnect/member/security/local/dto/AuthDto.java
@@ -109,6 +109,49 @@ public class AuthDto {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    public static class SignUpReqWithoutEmail {
+
+        // 회원 정보
+        @NotBlank(message = "아이디는 필수입니다")
+//        @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하여야 합니다")
+//        @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "아이디는 영문, 숫자, 언더스코어만 사용 가능합니다")
+        private String username;
+
+        @NotBlank(message = "비밀번호는 필수입니다")
+        @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다")
+        private String password;
+
+        @NotBlank(message = "비밀번호 확인은 필수입니다")
+        private String passwordConfirm;
+
+        // 기업 정보
+//        @NotBlank(message = "브랜드명은 필수입니다")
+//        private String brandName;
+
+        @NotBlank(message = "담당자 이름은 필수입니다")
+        private String managerName;
+
+        @NotBlank(message = "연락처는 필수입니다")
+//        @Pattern(regexp = "^01[0-9]-?\\d{3,4}-?\\d{4}$", message = "유효한 전화번호 형식이 아닙니다")
+//        private String phone;
+
+        @NotBlank(message = "이메일은 필수입니다")
+        @Email(message = "유효한 이메일 형식이 아닙니다")
+        private String email;
+
+
+        @NotNull(message = "사용자 역할은 필수입니다")
+        private UserRole Userrole;
+
+        @NotNull(message = "사용자 상태는 필수입니다")
+        private UserStatus UserStatus;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Builder
     public static class SignUpResp {
         private Long userId;
