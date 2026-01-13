70d14aacfa22baff6199e4fe9310b974216992ba
Dongyoon Chang <144019166+eastlight82@users.noreply.github.com>
2026-01-13 23:47:47 +0900

    fix: Add new CORS origin for uniconnect

--- src/main/java/com/uniConnect/config/CorsConfig.java
+++ src/main/java/com/uniConnect/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                 "http://localhost:8081",
                 "http://localhost:8080",
                 "http://10.0.2.2:8080",
-                "http://uniconnect.swagger.s3-website.ap-northeast-2.amazonaws.com"
+                "http://uniconnect.swagger.s3-website.ap-northeast-2.amazonaws.com", //안올려도 됐었음
+                "http://uniconnect-250909.s3-website.ap-northeast-2.amazonaws.com"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
