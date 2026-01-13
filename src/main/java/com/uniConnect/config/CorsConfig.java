d871d61f0e10ddae046b8e544c99b1b2dd1a4e2d
Dongyoon Chang <144019166+eastlight82@users.noreply.github.com>
2026-01-13 23:59:10 +0900

    fix: cors fe추가

--- src/main/java/com/uniConnect/config/CorsConfig.java
+++ src/main/java/com/uniConnect/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
                 "http://localhost:8080",
                 "http://10.0.2.2:8080",
                 "http://uniconnect.swagger.s3-website.ap-northeast-2.amazonaws.com", //안올려도 됐었음
-                "http://uniconnect-250909.s3-website.ap-northeast-2.amazonaws.com"
+                "http://uniconnect-250909.s3-website.ap-northeast-2.amazonaws.com",
+                "https://uniconnectcontact.vercel.app/"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
